33be5832ab1713c2d415e6e8fb644300849c833b
osh2004 <neosky1448@gmail.com>
2024-07-29 18:04:48 +0900

    비밀번호 변경 구현

--- src/main/java/com/project/car_Detail/user/UserRepository.java
+++ src/main/java/com/project/car_Detail/user/UserRepository.java
@@ -5,5 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-	Optional<SiteUser> findByUsername(String username);
+    Optional<SiteUser> findByUsername(String username);
+    Optional<SiteUser> findByEmail(String email); // 이메일로 사용자를 찾는 메서드 추가
 }
